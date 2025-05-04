ef14cfb0e38a8a9cf1e7121be63f6c845be25051
Ahmed Nabil <ahmed.nabil55586@gmail.com>
2025-05-04 20:48:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://picsum.photos/seed/img1/150/100) on `next/image`, hostname "picsum.photos" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: Call Stack
    > defaultLoader
    > webpack:/src/shared/lib/image-loader.ts
    > loader
    > webpack:/src/shared/lib/get-img-props.ts
    > Array.map
    > <anonymous>
    > map
    > webpack:/src/shared/lib/get-img-props.ts
    > generateImgAttrs
    > webpack:/src/shared/lib/get-img-props.ts
    > props
    > webpack:/src/client/image-component.tsx
    > React

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your existing Next.js config options here...
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**', // Allow any path under this hostname
+      },
+    ],
+  },
   webpack: (config, { isServer }) => {
-    // No longer need Konva specific aliases
-    // config.resolve.alias = {
-    //   ...config.resolve.alias,
-    //   canvas: false,
-    //   'konva$': 'konva/lib/index-browser',
-    // };
-
     // Important: return the modified config
     return config;
   },
