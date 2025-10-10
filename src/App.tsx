846e92c1a59f454401a4410e19f46c796ac07a09
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2025-10-10 18:21:14 +0300

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-// In App.tsx
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-
-// In components using useSearchParams
+import { useEffect } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LicenseAgreement from "./pages/LicenseAgreementPage";
 import PrivacyPolicy from "./pages/PrivacyPolicyPage";
 import TermsOfService from "./pages/TermsOfServicePage";
 
+function ScrollToHash() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      setTimeout(() => {
+        const element = document.querySelector(location.hash);
+        if (element) element.scrollIntoView({ behavior: "smooth" });
+      }, 200);
+    }
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToHash /> {/* ✅ listens for hash changes */}
       <div className="min-h-screen bg-white">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/license" element={<LicenseAgreement />} />
-          <Route path="//terms-of-service" element={<TermsOfService />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
         </Routes>
       </div>
     </Router>
